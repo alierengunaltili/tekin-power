9ddef16c81a4ee8d05cbab4c920b3b04067c5e1b
salihelcicek <salihultraslan56@gmail.com>
2025-09-22 20:33:44 +0300

    build fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Analytics from "@/components/Analytics";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -74,7 +73,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-gray-900`}
       >
-        <Analytics />
         {children}
       </body>
     </html>
